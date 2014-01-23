b0b45a023641618c7c35bf2fac312f5ffc917960
Luiz Cezer <lccezinha@gmail.com>
2014-01-23 01:09:18 -0200

    Fix problema in transfer

--- app/services/transfer_service.rb
+++ app/services/transfer_service.rb
@@ -10,9 +10,10 @@ class TransferService
       begin
         from = Account.find(@transfer.account_id)
         to   = Account.find(@transfer.account_destiny_id)
-        from.minus(@transfer.quantity)
-        to.plus(@transfer.quantity)
-        Movimentation.create!(account_id: from.id, account_destiny_id: to.id, quantity: @transfer.quantity, operation: @transfer.operation)
+        if from.minus(@transfer.quantity)
+          to.plus(@transfer.quantity)
+          Movimentation.create!(account_id: from.id, account_destiny_id: to.id, quantity: @transfer.quantity, operation: @transfer.operation)
+        end
       rescue => ex
         Rails.logger.info "Exception: #{ex.class}. Msg: #{ex.message}. Trace: #{ex.backtrace}"
         raise ActiveRecord::Rollback
--- spec/services/transfer_service_spec.rb
+++ spec/services/transfer_service_spec.rb
@@ -8,8 +8,9 @@ describe TransferService do
     let(:account) { create :account, user_id: user.id }
     let(:account_two) { create :account, user_id: user_two.id }
 
+    let(:quantity) { BigDecimal.new(50) }
+
     it 'create one Movimentation with operation: transfer' do
-      quantity = 50
       transfer = Transfer.new account_id: account.id, account_destiny_id: account_two.id,
         quantity: quantity
       transfer_service = TransferService.new transfer
@@ -18,20 +19,19 @@ describe TransferService do
     end
 
     it 'add quantity for in destiny account' do
-      quantity = 50
       transfer = Transfer.new account_id: account.id, account_destiny_id: account_two.id,
         quantity: quantity
       transfer_service = TransferService.new transfer
       expect {
         transfer_service.run
-      }.to change(account_two, :total).by(quantity.to_d)
+      }.to change(account_two.reload, :total).by(quantity)
     end
 
     it 'account_id and account_destiny_id can not be equal' do
       account = create :account
       account_two = create :account
       transfer = Transfer.new account_id: account.id,
-        account_destiny_id: account.id, quantity: 50
+        account_destiny_id: account.id, quantity: quantity
       expect(transfer).not_to be_valid
       expect(transfer.errors.keys).to include(:account_destiny_id)
       expect(transfer.errors[:account_destiny_id]).to include('Conta destino não pode ser a conta de origem')
