8a51c21e82afe54e27ae3c49781aa02edf019662
Luiz Cezer <lccezinha@gmail.com>
2014-06-13 20:00:53 -0300

    mais configs de deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'minibank'
-set :repo_url, ' https://lccezinha@bitbucket.org/lccezinha/bank.git'
+set :repo_url, 'git@bitbucket.org:lccezinha/bank.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -11,7 +11,7 @@ set :repo_url, ' https://lccezinha@bitbucket.org/lccezinha/bank.git'
 set :deploy_to, '/var/www/minibank'
 
 # Default value for :scm is :git
-set :git
+set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
@@ -26,7 +26,7 @@ set :format, :pretty
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
-role :app, %w[cezer@162.243.227.29]
+role :app, %w{cezer@162.243.227.29}
 set :ssh_options, forward_agent: true
