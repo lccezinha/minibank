b3bf015f723394a77792f4ad1e7dc4a3ba896c62
Luiz Cezer <lccezinha@gmail.com>
2014-06-12 23:25:58 -0300

    setup do capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,13 +8,13 @@ set :repo_url, ' https://lccezinha@bitbucket.org/lccezinha/bank.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/minibank'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
@@ -23,10 +23,10 @@ set :repo_url, ' https://lccezinha@bitbucket.org/lccezinha/bank.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
