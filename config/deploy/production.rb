8c9a85574fe428c9432e549834160a97945d989a
Luiz Cezer <lccezinha@gmail.com>
2014-06-30 22:13:59 -0300

    Configs p/ deploy staging/production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
+set :branch, 'staging' # vai pegar da staging
 role :app, %w{cezer@162.243.227.29}
 role :web, %w{cezer@162.243.227.29}
 role :db, %w{cezer@162.243.227.29}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,5 @@
 set :stage, :staging
+set :branch, 'development' # vaí pegar da dev
 role :app, %w{cezer@104.131.240.101}
 role :web, %w{cezer@104.131.240.101}
 role :db, %w{cezer@104.131.240.101}
